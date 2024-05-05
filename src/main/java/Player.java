040fa6048b2649274b2a23cc18b7fbf91ca0af00
Gourav <gourav@sambuq.com>
2024-05-05 16:03:33 +0530

    added player entity

---
+++ src/main/java/Player.java
@@ -0,0 +1,79 @@
+package main.java;
+
+public class Player {
+    private String name;
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(String name, int health, int strength, int attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    public int getAttackDamage(int diceRoll) {
+        return attack * diceRoll;
+    }
+
+    public int getDefenseStrength(int diceRoll) {
+        return strength * diceRoll;
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", strength=" + strength +
+                ", attack=" + attack +
+                '}';
+    }
+}
+
